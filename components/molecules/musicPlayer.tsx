4087e04a31c8f4d1091b79f2873a81002cf41ef5
zapagenrevdale <zapagenrevdale@gmail.com>
2023-06-18 17:13:18 +0800

    Apply animation on close

--- components/molecules/musicPlayer.tsx
+++ components/molecules/musicPlayer.tsx
@@ -20,18 +20,20 @@ const App: React.FC = () => {
 
   return (
     <div className="flex items-center space-x-2">
-      {isPlaying && (
-        <motion.div
-          className="font-xs font-quickSand flex items-center justify-center bg-custom-brown/60 text-white shadow-sm rounded-l-full p-2 px-4 h-10 truncate overflow-hidden border-r-4 border-r-custom-brown"
-          animate={{ width: ["0px", "250px"] }}
-          transition={{
-            duration: 0.5,
-            ease: "linear",
-          }}
-        >
-          <MusicalNoteIcon className="h-4 w-4 mr-2" /> Pasilyo - SunKissed Lola
-        </motion.div>
-      )}
+      <motion.div
+        className="font-xs font-quickSand flex items-center justify-center bg-custom-brown/60 text-white shadow-sm rounded-l-full p-2 px-4 h-10 truncate overflow-hidden border-r-4 border-r-custom-brown"
+        animate={{
+          width: isPlaying ? ["0px", "250px"] : ["250px", "0px"],
+          opacity: isPlaying ? 100 : 0,
+        }}
+        transition={{
+          duration: 0.5,
+          ease: "linear",
+        }}
+      >
+        <MusicalNoteIcon className="h-4 w-4 mr-2" /> Pasilyo - SunKissed Lola
+      </motion.div>
+
       <div>
         <audio ref={audioRef}>
           <source src="/music/pasilyo.mp3" type="audio/mpeg" />
