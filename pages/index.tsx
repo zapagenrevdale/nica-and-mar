4d9a7659d4f9f390a35650351227ee63acd62d64
zapagenrevdale <zapagenrevdale@gmail.com>
2023-07-08 22:18:10 +0800

    Update website title

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,18 @@ import Divider from "@/components/atoms/divider";
 import MainPhotos from "@/components/molecules/mainPhotos";
 import FAQS from "@/components/organisms/faqs";
 import Guide from "@/components/organisms/guide";
+import Head from "next/head";
 const Home = () => {
   return (
     <main className="flex flex-col items-center w-full pb-10">
+      <Head>
+        <title>Nica and Mar</title>
+        <meta
+          name="description"
+          content="Nica and Mar Wedding Invitation! #NicaTadhanaKayMar"
+        />
+        <meta name="robots" content="index, follow" />
+      </Head>
       <Hero />
       <div className="container relative">
         {/* now image with link  */}
